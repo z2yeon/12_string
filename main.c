ef28298eed71f8252941fd1c56474c30a1d9609c
z2yeon <55427811+z2yeon@users.noreply.github.com>
2019-11-19 15:29:07 +0900

    12_03

--- main.c
+++ main.c
@@ -1,24 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
 	
-	int i=0;
-	char str[4];
+	char src[]="The worst things to eat before you sleep";
+	char dst[100];
 	
-	str[0]='a';
-	str[1]='b';
-	str[2]='c';
-	str[3]='\0';
+	strcpy(dst,src);
 	
-	printf("%s\n",str);
-	
-	while (str[i]!='\0')
-	{	
-		printf("%c",str[i]);
-		i++;
-	}
+	printf("copied string:%s",dst);
 	return 0;
 }
