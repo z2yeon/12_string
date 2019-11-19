52e9bc4746c5b92e4a5e0d672cf6c7bb104d00b6
z2yeon <55427811+z2yeon@users.noreply.github.com>
2019-11-19 15:21:06 +0900

    12_02

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int i=0;
+	char str[4];
+	
+	str[0]='a';
+	str[1]='b';
+	str[2]='c';
+	str[3]='\0';
+	
+	printf("%s\n",str);
+	
+	while (str[i]!='\0')
+	{	
+		printf("%c",str[i]);
+		i++;
+	}
+	return 0;
+}
